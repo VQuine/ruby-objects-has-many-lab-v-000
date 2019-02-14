c9c3d084b454725a734ab24d15f43d387ac25e2f
Kimberly Aviles <01010110.v@gmail.com>
2019-02-14 05:06:01 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -12,11 +12,12 @@ class Post
   end
 
   def author_name
-  if @name == nil
-    return nil
-  else
-    return self.author.name
-  end
+    @name
+  # if @name == nil
+  #   return nil
+  # else
+  #   return self.author.name
+  # end
   #self.author.name #<---assigns the name attritube (in the author file) to the author attritube, to the instantiated object self [defining the Post class objects' author]
 
   end
